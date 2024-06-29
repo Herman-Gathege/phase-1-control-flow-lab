9b77492735cbb89fbf72c45f55035ca79261b2ff
Herman-Gathege <siinamtechstudio@gmail.com>
2024-06-29 12:05:11 +0300

    change new

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+    if (feet <= 400) {  
+      console.log('This one is on me!');
+  }  if (400, 2000) {
+    console.log('That will be twenty bucks.');
+  }  if (2000, 2001 ) {
+    console.log('I will gladly take your thirty bucks.');
+  }  if (2500, 2501 ) {
+    console.log('No can do.');
+  }
 }
+scuberGreetingForFeet();
 
 function ternaryCheckCity(){
   // Write your code here!
+  if (city === NYC) {
+    console.log('Ok, sounds good.');
+  } 
+  if (city != NYC) {
+    console.log('No go.');
+  }
 }
+ternaryCheckCity();
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  if (tip === generous) {
+    console.log('Thank you so much.');
+  }
+  if (tip !== generous) {
+    console.log('Thank you.');
+  }
+  else {
+    console.log('Bye.');
+  }
 }
+switchOnCharmFromTip();
